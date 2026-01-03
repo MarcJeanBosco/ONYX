be27e24c022db5b5b5ccf05a30e2b199a26cdac5
Bosco <ibrahimfmarcjeanbosco@gmail.com>
2026-01-03 11:37:58 +0300

    premier commit

---
+++ js/onyx.js
@@ -0,0 +1,96 @@
+let nbr1=4;
+let nbr2=10;
+let result= nbr1+nbr2
+
+console.log(result)
+
+// for (let i=0; i<10; i++){
+//    console.log("Bonjour" +i)
+// }
+
+
+let g=1;
+ do{
+    console.log("aurevoir" +g);
+    g++;
+ }
+ while (g<10)
+
+let j=10;
+while(j>6){
+    console.log("veloma" +j);
+    j--;
+}
+    
+let age =14;
+
+if (age >18) {
+    console.log("vous pouver entrer")
+    
+} else {
+    
+}
+
+ let matiere=["svt","maths","ang","hg","pc","ml"];
+
+let long=matiere.length
+console.log(matiere.length)
+
+for(let y=0; y<matiere.length; y++)
+    console.log (matiere[y])
+ 
+
+let contact = ["Doda","Bob","James","Randria","Fanyah"];
+let contactRevert = [];
+let l = 0;
+let total = contact.length;
+console.log(total)
+for(let k=total-1; k >= 0 ;k--){
+    contactRevert[l]=contact[k]
+    console.log(contactRevert)
+    l=l+1
+}
+contact = contactRevert
+
+
+
+// let total = 0;
+// while(contact[total]){
+//     total = total+1
+// } '5' 5
+
+
+// console.log(total)
+
+// let ruche=["reine","faux bourdon"]
+
+//     let e=ruche[0]
+//     ruche[0]= ruche[1]
+//     ruche[1]=e
+
+// console.log(ruche)
+
+
+// function calculeAge(anneN,anneA) {
+     
+//     //const anneA=2026;
+//     let monAge;
+
+//     monAge= anneA-anneN;
+//     console.log(monAge);
+//     return monAge;
+// }
+
+// calculeAge(1999,2004)
+
+
+// function boucleBonjour(params) {
+
+//    for (let i=0; i<10; i++){
+//    !
+// } 
+// }
+// boucleBonjour()
+
+
+
---
+++ js/testfonction.js
@@ -0,0 +1,11 @@
+function calculeAge(anneN,anneA) {
+     
+    //const anneA=2026;
+    let monAge;
+
+    monAge= anneA-anneN;
+    console.log(monAge);
+    return monAge;
+}
+
+calculeAge(1999,2004);
